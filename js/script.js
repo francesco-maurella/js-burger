cb14bc4c34211720b0ab6abb35870ed69b98c213
Francesco Maurella <francesco.maurella@live.it>
2020-12-21 17:14:00 +0100

    Checkbox ingredients list

--- js/script.js
+++ js/script.js
@@ -2,6 +2,28 @@ var order = document.getElementsByClassName('order')[0];
 
 var orderName = order.getElementsByTagName('input')[0];
 
-var orderIngredients = order.getElementsByClassName('className')[0];
+var orderIngredients = order.getElementsByClassName('order-ingredients')[0];
 
 var ingredient = orderIngredients.getElementsByTagName('input');
+
+var coupon = order.getElementsByTagName('input')[1];
+
+var submit = order.getElementsByTagName('button')[0];
+
+var choice = 0;
+
+var totalPrice = document.getElementById('price');
+
+submit.addEventListener("click", function() {
+  if (!orderName.value) {
+    alert('Name your Burger, please!');
+  } else {
+    for (var i = 0; i < ingredient.length; i++) {
+      if (ingredient[i].checked) {
+        choice += parseInt(ingredient[i].value);
+      }
+    }
+    totalPrice.innerText = choice;
+  }
+}
+);
