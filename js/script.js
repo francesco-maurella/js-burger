1ff4b1e186c1719dcef2839114546b56bdc24ca6
Francesco Maurella <francesco.maurella@live.it>
2020-12-21 16:08:47 +0100

    Creazione files e strutture (Html, css, js)

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var order = document.getElementsByClassName('order')[0];
+
+var orderName = order.getElementsByTagName('input')[0];
+
+var orderIngredients = order.getElementsByClassName('className')[0];
+
+var ingredient = orderIngredients.getElementsByTagName('input');
