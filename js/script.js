ff3be92ec98ff66ba233b1296e8e6359413d04e2
Francesco Maurella <francesco.maurella@live.it>
2020-12-22 12:49:59 +0100

    Refactoring script

--- js/script.js
+++ js/script.js
@@ -20,44 +20,41 @@ var coupon;
 
 // Button after-click function
 submit.addEventListener("click", function() {
+  // assign value at Undefined vars, for reset them after every click
+  checked = [];
+  choice = 0;
 
+  // checked ingredients
+  for (var i = 0; i < ingredient.length; i++) {
+    if (ingredient[i].checked) {
+      // if ingredient is .checked, pushing its value into "checked" array
+      checked.push(ingredient[i].value);
+      // sum ingredients checked values
+      choice += parseInt(ingredient[i].value);
+    }
+  }
   // verify IF the order has a name
   if (!orderName.value) {
     alert('Name your Burger, please!');
+  // verify IF ingredients are 2 or more
+  } else if (checked.length < 2) {
+    alert('Choose 2 or more ingredients');
   } else {
 
-    // assign value at Undefined vars, for reset them after every click
-    checked = [];
-    choice = 0;
+    /* ----- alternative method to sum ingredients, using array -----
+    for (var i = 0; i < checked.length; i++) {
+    choice += parseInt(checked[i]); }
+    ----------------------------------------------------------------*/
 
-    // checked ingredients
-    for (var i = 0; i < ingredient.length; i++) {
-      if (ingredient[i].checked) {
-        // count ingredients - pushing their values into "checked" array
-        checked.push(ingredient[i].value);
-        // sum ingredient values
-        choice += parseInt(ingredient[i].value);
-      }
+    // assign coupon var value here, for refresh it, after every click
+    coupon = document.getElementById('coupon');
+    // verify coupon code
+    if (coupon.value === 'M2ERRY0CHR2ISTM0AS') {
+      choice = choice - (choice * 20 / 100);
     }
-    // verify IF ingredients are 2 or more
-    if (checked.length < 2) {
-      alert('Choose 2 or more ingredients');
-    } else {
-      // for (var i = 0; i < checked.length; i++) {
-      //   choice += parseInt(checked[i]);
-      // }
 
-      // assign coupon var value here, for refresh it, after every click
-      coupon = document.getElementById('coupon');
-      // verify coupon code
-      if (coupon.value === 'M2ERRY0CHR2ISTM0AS') {
-        coupon = choice * 20 / 100;
-        choice = choice - coupon;
-      }
-
-      // print the result
-      totalPrice.innerText = choice;
-    }
+    // print the result
+    totalPrice.innerText = 'Eur.' + parseInt(8 + choice);
   }
 }
 );
