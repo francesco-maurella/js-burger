b1c489d8133b273f34ed2c07be69f3b3a7899d74
Francesco Maurella <francesco.maurella@live.it>
2020-12-22 17:50:34 +0100

    Stile complessivo completo

--- js/script.js
+++ js/script.js
@@ -1,42 +1,41 @@
-// Vars definitions
-var order = document.getElementsByClassName('order')[0];
+// Order-name var input - definition
+var orderName = document.getElementsByClassName('order-name')[0].getElementsByTagName('input')[0];
 
-var orderName = order.getElementsByTagName('input')[0];
+// Order-ingredients var inputs - definition
+var orderIngredients = document.getElementsByClassName('order-ingredients')[0].getElementsByTagName('input');
 
-var orderIngredients = order.getElementsByClassName('order-ingredients')[0];
+// Submit button var input - definition
+var submit = document.getElementsByClassName('order-submit')[0].getElementsByTagName('input')[0];
 
-var ingredient = orderIngredients.getElementsByTagName('input');
-
-var submit = order.getElementsByTagName('button')[0];
-
+// Footer containers vars - definition
+var user = document.getElementById('user');
 var totalPrice = document.getElementById('price');
 
 // Undefined Vars - use them in the "Button after-click function"
-var choice;
-
 var checked;
-
 var coupon;
+var price;
+var discount;
 
 // Button after-click function
 submit.addEventListener("click", function() {
-  // assign value at Undefined vars, for reset them after every click
+  // assign here value at Undefined vars, for reset them after every click
   checked = [];
-  choice = 0;
+  price = 0;
 
   // checked ingredients
-  for (var i = 0; i < ingredient.length; i++) {
-    if (ingredient[i].checked) {
-      // if ingredient is .checked, pushing its value into "checked" array
-      checked.push(ingredient[i].value);
+  for (var i = 0; i < orderIngredients.length; i++) {
+    if (orderIngredients[i].checked) {
+      // if orderIngredients is .checked, pushing its value into "checked" array
+      checked.push(orderIngredients[i].value);
       // sum ingredients checked values
-      choice += parseInt(ingredient[i].value);
+      price += parseInt(orderIngredients[i].value);
     }
   }
-  // verify IF the order has a name
+  // order name verification
   if (!orderName.value) {
     alert('Name your Burger, please!');
-  // verify IF ingredients are 2 or more
+  // ingredients quantity verificatio (2 or more)
   } else if (checked.length < 2) {
     alert('Choose 2 or more ingredients');
   } else {
@@ -46,15 +45,20 @@ submit.addEventListener("click", function() {
     choice += parseInt(checked[i]); }
     ----------------------------------------------------------------*/
 
-    // assign coupon var value here, for refresh it, after every click
+    // assign here coupon value for refresh it, after every click
     coupon = document.getElementById('coupon');
     // verify coupon code
     if (coupon.value === 'M2ERRY0CHR2ISTM0AS') {
-      choice = choice - (choice * 20 / 100);
+      discount = 8 + price - (price * 20 / 100);
+      // print discounted price
+      totalPrice.innerHTML = 'Eur.' + '<small><s> ' + parseInt(8 + price) + '</s>  (-20%)</small> ' + discount;
+    } else {
+      // print normal price
+      totalPrice.innerText = 'Eur.' + parseInt(8 + price);
     }
 
-    // print the result
-    totalPrice.innerText = 'Eur.' + parseInt(8 + choice);
+    // print order name
+    user.innerHTML = '#' + orderName.value + '<small> price</small>';
   }
 }
 );
